5a6b17c11d00bdb6f1fbf455423201d42ba4bb8e
mihapek <pekarskij@gmail.com>
2025-05-27 01:22:55 +0200

    Add environment configuration files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  chatApiUrl: 'https://api.openai.com/v1/chat/completions',
+  imageApiUrl: 'https://api.openai.com/v1/images/generations',
+  apiKey: '',
+  dbApiUrl: 'https://parseapi.back4app.com/classes/Profile',
+  dbApiKey: '',
+  dbAppId: ''
+};
